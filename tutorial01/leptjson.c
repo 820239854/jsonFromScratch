bbe31905ba2bf0292751155d2710dc7c134071e7
Archer <820239854@qq.com>
2023-04-13 12:16:34 +0800

    代码格式化

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -1,21 +1,36 @@
 #include "leptjson.h"
-#include <assert.h>  /* assert() */
-#include <stdlib.h>  /* NULL */
+#include <assert.h> /* assert() */
+#include <stdlib.h> /* NULL */
 
-#define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+// 在 C 语言中，箭头运算符 -> 与 解引用运算符 * 的优先级哪个高？
 
-typedef struct {
-    const char* json;
-}lept_context;
+// 在 C 语言中，箭头运算符 -> 的优先级比解引用运算符 * 的优先级高。
+// 也就是说，对于形如 a->b 的表达式，编译器会先执行箭头运算符，即先获取指针 a 所指向的结构体对象，然后再访问该对象的成员变量 b。
+// 因此，在本题中的表达式 *c->json 中，-> 运算符会先被执行，
+// 将指针 c 所指向的结构体对象中的 json 成员变量取出来，然后再使用解引用运算符 * 对其进行解引用操作，得到这个字符指针所指向的字符。
+// 最后，再将这个字符与 (ch) 进行相等性比较。
+#define EXPECT(c, ch)             \
+    do                            \
+    {                             \
+        assert(*c->json == (ch)); \
+        c->json++;                \
+    } while (0)
 
-static void lept_parse_whitespace(lept_context* c) {
+typedef struct
+{
+    const char *json;
+} lept_context;
+
+static void lept_parse_whitespace(lept_context *c)
+{
     const char *p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
         p++;
     c->json = p;
 }
 
-static int lept_parse_null(lept_context* c, lept_value* v) {
+static int lept_parse_null(lept_context *c, lept_value *v)
+{
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
@@ -24,15 +39,21 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
-static int lept_parse_value(lept_context* c, lept_value* v) {
-    switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+static int lept_parse_value(lept_context *c, lept_value *v)
+{
+    switch (*c->json)
+    {
+    case 'n':
+        return lept_parse_null(c, v);
+    case '\0':
+        return LEPT_PARSE_EXPECT_VALUE;
+    default:
+        return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
-int lept_parse(lept_value* v, const char* json) {
+int lept_parse(lept_value *v, const char *json)
+{
     lept_context c;
     assert(v != NULL);
     c.json = json;
@@ -41,7 +62,8 @@ int lept_parse(lept_value* v, const char* json) {
     return lept_parse_value(&c, v);
 }
 
-lept_type lept_get_type(const lept_value* v) {
+lept_type lept_get_type(const lept_value *v)
+{
     assert(v != NULL);
     return v->type;
 }
